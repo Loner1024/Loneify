8aa16162da477f299e1320130e51fb476d2e8714
Loner1024 <xth12138@gmail.com>
2022-05-03 21:20:45 +0800

    move

---
+++ react-tailwind-agency/src/widgets/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function hero() {
+  return (
+    <div className="grid justify-items-start gap-8 pb-28 relative">
+      <p className="text-4xl md:text-6xl font-black text-center ledaing-noraml md:leading-normal"></p>
+    </div>
+  )
+}
+
+export default hero
